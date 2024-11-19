7338132be6e4d443a824972f8554e4537b64d348
ymatsu-suke <matsuzaki.y.a1@gmail.com>
2024-11-19 17:09:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-const projects = [
-  {
-    title: "New Project",
-    image: "images/new-project.jpg",
-    url: "new-project.html",
-  },
-];
+// Google Analytics initialization
+window.ga = window.ga || function() {
+  (ga.q = ga.q || []).push(arguments);
+};
+ga.l = +new Date;
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
 
-const worksList = document.getElementById("works-index");
-projects.forEach((project) => {
-  const projectItem = document.createElement("div");
-  projectItem.classList.add("p-worksIndex__listItem", "c-cardWorks", "worksCard");
-  projectItem.innerHTML = `
-    <a href="${project.url}">
-      <div class="c-cardWorks__thumb">
-        <img src="${project.image}" alt="${project.title}">
-      </div>
-      <div class="c-cardWorks__info">
-        <h3 class="c-cardWorks__ttl">${project.title}</h3>
-      </div>
-    </a>
-  `;
-  worksList.appendChild(projectItem);
+// Font loading
+WebFont.load({
+  google: {
+    families: ['Noto+Serif+JP:400,700', 'Libre+Baskerville']
+  }
 });
